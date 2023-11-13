9cf68cdce904a7f091a67b35e0b43ec1fedc8a50
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-14 00:47:33 +0600

    Added content in Modal.tsx, added onClose (React.MouseEventHandler)

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,16 +7,21 @@ interface Props extends React.PropsWithChildren  {
   mainTitle: string;
   onClose: React.MouseEventHandler;
 }
-const Modal:React.FC<Props> = ({show, mainTitle, onClose, children }) => {
+const Modal:React.FC<Props> = ({show, mainTitle, onClose,children }) => {
 
   return (
     <>
-      <Backdrop show={show} onClick={onClose}/>
-      <div className="modal show" style={{display: show ? 'block': 'none'}} onClick={onClose}>
+      <Backdrop show={show}/>
+      <div className="modal show" style={{display: show ? 'block': 'none'}} >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5">{mainTitle}</h1>
+              <button type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                      onClick={onClose}/>
             </div>
             {children}
           </div>
