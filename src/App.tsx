8c961aa346f41a8544b49ca27370242c933296da
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-14 00:48:43 +0600

    Created state for show Modal, and added button to close modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 
 import './App.css';
+import Modal from './components/Modal/Modal';
+import {useState} from 'react';
+
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-
+      <div>
+        <Modal show={showModal} mainTitle="Some kinda modal title" onClose={() => setShowModal(false)}>
+        <p className="modal-body">
+          This is modal content
+        </p>
+        </Modal>
+        <button className="w-100 btn btn-light" onClick={() => setShowModal(true)}>Demonstrate</button>
+      </div>
     </>
   );
 }
